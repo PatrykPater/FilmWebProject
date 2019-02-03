323fc6759fd21c24d193f70443c73c48bde85bdb
Patryk Pater <paterp989@gmail.com>
2019-02-03 15:20:01 +0100

    populate genres table

---
+++ FilmWebProject/Migrations/201902031327481_PopulateGenres.cs
@@ -0,0 +1,29 @@
+namespace FilmWebProject.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateGenres : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genres (Name) VALUES ('Action')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Animated')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Documentary')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Drama')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Family')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Fantasy')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Horror')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Comedy')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Crime')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Silent')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Adventure')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Romance')");
+            Sql("INSERT INTO Genres (Name) VALUES ('Sci-fi')");
+        }
+
+        public override void Down()
+        {
+            Sql("DELETE FROM Genres WHERE Name IN ('Action', 'Animated', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Horror', 'Comedy', 'Crime', 'Silent' , 'Adventure', 'Romance', 'Sci-fi')");
+        }
+    }
+}
