5cf23ea936afbe797f73382e22b8744c7612d973
Patryk Pater <paterp989@gmail.com>
2019-02-03 09:55:26 +0100

    Add project files.

---
+++ FilmWebProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilmWebProject.Startup))]
+namespace FilmWebProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
