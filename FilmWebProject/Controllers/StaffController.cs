ce2ab584fbb11b42ef4f6c2548c5ba8a119cb747
Patryk Pater <paterp989@gmail.com>
2019-02-04 14:08:01 +0100

    maker/actor form initial

---
+++ FilmWebProject/Controllers/StaffController.cs
@@ -0,0 +1,23 @@
+﻿using FilmWebProject.Core.ViewModels;
+using FilmWebProject.Persistance;
+using System.Web.Mvc;
+
+namespace FilmWebProject.Controllers
+{
+    public class StaffController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public StaffController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new CreateStaffFormViewModel();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ FilmWebProject/Core/ViewModels/CreateStaffFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FilmWebProject.Core.ViewModels
+{
+    public class CreateStaffFormViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+        public int Height { get; set; }
+    }
+}
