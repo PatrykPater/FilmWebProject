1809ba7e3a43b83d24d8982518740222de446702
Patryk Pater <paterp989@gmail.com>
2019-02-19 16:41:43 +0100

    fix list of film view, added a table, initial viewmodel for list of films

---
+++ FilmWebProject/Core/ViewModels/FilmListViewMOdel.cs
@@ -0,0 +1,21 @@
+﻿using FilmWebProject.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace FilmWebProject.Core.ViewModels
+{
+    public class FilmListViewModel
+    {
+        public string Title { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string ShortDescription { get; set; }
+        public string Production { get; set; }
+        public DateTime Release { get; set; }
+        public double Budget { get; set; }
+        public double Score { get; set; }
+        public string Studio { get; set; }
+        public double BoxOffice { get; set; }
+        public ICollection<Genre> Genres { get; set; }
+
+    }
+}
