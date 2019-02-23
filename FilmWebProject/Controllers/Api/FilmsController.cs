9c6b62a381072ffb7bcbf633d5fd9ebe926da891
Patryk Pater <paterp989@gmail.com>
2019-02-23 12:11:20 +0100

    implemented delete action, needs further work

--- FilmWebProject/Controllers/Api/FilmsController.cs
+++ FilmWebProject/Controllers/Api/FilmsController.cs
@@ -1,31 +1,28 @@
-﻿using AutoMapper;
-using Library.API.Entities;
-using Library.API.Helpers;
-using Library.API.Models;
-using Library.API.Services;
-using Microsoft.AspNetCore.JsonPatch;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using FilmWebProject.Persistence;
+using System.Linq;
 using System.Web.Http;
-using FilmWebProject.Persistence;
-using Microsoft.Extensions.Logging;
 
 namespace FilmWebProject.Controllers.Api
 {
     [Authorize]
     public class FilmsController : ApiController
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public FilmsController()
         {
             _context = new ApplicationDbContext();
         }
 
+        [HttpDelete]
         public IHttpActionResult Delete(int id)
         {
-            return NoContent();
+            var film = _context.Films.Single(f => f.Id == id);
+
+            _context.Films.Remove(film);
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
