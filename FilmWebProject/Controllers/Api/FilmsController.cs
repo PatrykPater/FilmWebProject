88172d267383d5046fee362bbcf50828016dc28d
Patryk Pater <paterp989@gmail.com>
2019-02-23 10:35:20 +0100

    film api, delete action initial

---
+++ FilmWebProject/Controllers/Api/FilmsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Library.API.Entities;
+using Library.API.Helpers;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using FilmWebProject.Persistence;
+using Microsoft.Extensions.Logging;
+
+namespace FilmWebProject.Controllers.Api
+{
+    [Authorize]
+    public class FilmsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FilmsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
