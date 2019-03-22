ac04ef9fd97d6edfc05b72642a36e024d318ded8
Patryk Pater <paterp989@gmail.com>
2019-03-22 12:53:22 +0100

    implemented interface for searching users

---
+++ Web/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SearchUser(string query = null)
+        {
+            var viewModel = new SearchUserViewModel();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Web/ViewModels/SearchUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Model.Models;
+using System.Collections.Generic;
+
+namespace Web.ViewModels
+{
+    public class SearchUserViewModel
+    {
+        public SearchUserViewModel()
+        {
+            Users = new List<ApplicationUser>();
+        }
+        public string Query { get; set; }
+        public ICollection<ApplicationUser> Users { get; set; }
+    }
+}
