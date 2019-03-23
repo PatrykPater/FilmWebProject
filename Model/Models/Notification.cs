a78ca695b1e31ab6d0f0f0b1d76d5e44b5ead9a9
Patryk Pater <paterp989@gmail.com>
2019-03-23 10:09:40 +0100

    added bool attribute at notification to determine if a message was read, added initial partial view for notifications

--- Model/Models/Notification.cs
+++ Model/Models/Notification.cs
@@ -5,8 +5,11 @@ namespace Model.Models
     public class Notification
     {
         public Guid Id { get; set; }
+        public bool IsRead { get; set; } = false;
 
         public ApplicationUser Recipient { get; set; }
         public ApplicationUser Sender { get; set; }
+
+
     }
 }
