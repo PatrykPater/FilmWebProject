9e76f90c2ca76ed7464152cbf0e32294a98c5c6a
Patryk Pater <paterp989@gmail.com>
2019-04-16 11:35:54 +0200

    initial work on admin panel and moderator role. Added moderator role, added admin controller which will contain all actions available to admin, added the ability to promote a user to moderator, added initial views for admin panel and mod promotion

---
+++ Web/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public ActionResult Panel()
+        {
+            var user = User.IsInRole("Admin");
+
+            if (!user)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "You are not authorized to access this page");
+
+            return View();
+        }
+
+        public ActionResult AddModerator(string email = null)
+        {
+            var user = User.IsInRole("Admin");
+
+            if (!user)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "You are not authorized to access this page");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(string email)
+        {
+
+            return RedirectToAction("AddModerator", "Admin", new { email });
+        }
+    }
+}
